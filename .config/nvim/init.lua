766935f8f0284347e7c4c37e7f4bd1b09f63a6e6
Fredericko XG <fredericko.goncalves@argointeligencia.com.br>
2022-11-23 11:35:22 -0400

    Restore cursor shape after exiting neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 require('user.options')
 require('user.keymaps')
 require('user.plugins')
+require('user.autocommands')
 require('user.colorscheme')
 require('user.lualine')
 require('user.nvim-tree')
